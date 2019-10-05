abedc2becf45787dd46d299b6db4c8ca401f4a19
SzymonSt <szymonst2808@gmail.com>
2019-10-05 14:38:39 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import {MatInputModule} from '@angular/material/input';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +16,7 @@ import { StoreModule } from '@ngrx/store';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule, AuthModule, StoreModule.forRoot({}), MatInputModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule, AuthModule, StoreModule.forRoot({}),],
   providers: [
     StatusBar,
     SplashScreen,
