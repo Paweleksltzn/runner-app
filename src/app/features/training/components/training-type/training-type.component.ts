52f69341ed4e5b9834dc0c3d4ee9c885232dcdee
SzymonSt <szymonst2808@gmail.com>
2019-10-05 13:30:46 +0200

    goals select

--- src/app/features/training/components/training-type/training-type.component.ts
+++ src/app/features/training/components/training-type/training-type.component.ts
@@ -1,28 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
+
+
 @Component({
   selector: 'app-training-type',
   templateUrl: './training-type.component.html',
   styleUrls: ['./training-type.component.scss'],
 })
 export class TrainingTypeComponent implements OnInit {
-  
+  public sportDyscypline: any;
+  public trainingGoalsSelects = [
+    {goalValue: 'kcal', goalText: 'Kalorie do spalenia'},
+    {goalValue: 'km', goalText: 'Kilometry do przebiegnięcia'},
+    {goalValue: 'min', goalText: 'Czas do pokonania'},
+  ];
 
   constructor(public actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {}
+
+
   async activityMenu() { 
     const actSheet = await this.actionSheetCtrl.create({
       header: 'Dyscypliny',
       buttons: [{
         text: 'Bieg',
-        icon: 'walk'
+        icon: 'walk',
+        handler: () => {
+          this.sportDyscypline = 'run';
+          console.log(this.sportDyscypline);
+        }
       }, 
       {
         text: 'Kolarstwo',
-        icon: 'bicycle'
+        icon: 'bicycle',
+        handler: () => {
+          this.sportDyscypline = 'bike';
+          console.log(this.sportDyscypline);
+        }
       },]
     });
     await actSheet.present();
   }
+
 }
