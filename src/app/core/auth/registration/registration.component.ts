a9661afddd8df1dc00bcad238de01123c7365698
SzymonSt <szymonst2808@gmail.com>
2019-10-08 22:43:53 +0200

    Custom sex checker
    -Made custom checker insted of this fookin ion-segment
    -More space near to reg button

--- src/app/core/auth/registration/registration.component.ts
+++ src/app/core/auth/registration/registration.component.ts
@@ -6,9 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
-
+  public sexChecked = 0;
+  // 0 => nobody is checked
+  // 1 => male is checked
+  // 2 => female is checked
   constructor() { }
 
   ngOnInit() {}
+  emphasizeIconM() {
+    const male = document.getElementById('sexMale');
+    const female = document.getElementById('sexFemale');
+    if (this.sexChecked === 0 || this.sexChecked === 2 ) {
+      male.style.borderBottom = '3px solid #555';
+      female.style.borderBottom = 'none';
+      this.sexChecked = 1;
+    }
+  }
+  emphasizeIconF() {
+    const male = document.getElementById('sexMale');
+    const female = document.getElementById('sexFemale');
+    if (this.sexChecked === 0 || this.sexChecked === 1 ) {
+      female.style.borderBottom = '3px solid #555';
+      male.style.borderBottom = 'none';
+      this.sexChecked = 2;
+    }
+  }
 
 }
