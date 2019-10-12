ab9e5429a7bcc9f7de9d88fcd44c8a85c70bd837
Paweł <pawelantolak@interia.eu>
2019-10-12 12:44:11 +0200

    import clear

--- src/app/core/auth/store/auth.reducer.ts
+++ src/app/core/auth/store/auth.reducer.ts
@@ -1,6 +1,4 @@
-import { Coordinates } from '../../../shared/interfaces/coordinates';
 import { createReducer, on, Action } from '@ngrx/store';
-import { TrainingState } from '../../../shared/interfaces/trainingState';
 import { actions } from '../../../store';
 import { AuthState } from 'src/app/shared/interfaces/auth/AuthState';
 
