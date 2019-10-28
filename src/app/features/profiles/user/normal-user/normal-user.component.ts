976521ed1b39c8f5697adc832ff44d6ccfdea2b4
SzymonSt <szymonst2808@gmail.com>
2019-10-28 20:38:54 +0100

    Editing profile photos started

--- src/app/features/profiles/user/normal-user/normal-user.component.ts
+++ src/app/features/profiles/user/normal-user/normal-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-normal-user',
@@ -10,17 +12,46 @@ export class NormalUserComponent implements OnInit {
   public userName: string;
   public userSurname: string;
   public selectedProfileTab: number;
-
-  constructor() { }
+  public camera: Camera;
+  constructor(public actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
-    this.userName = "Jacek"
-    this.userSurname = "Soplica"
+    this.userName = "Jacek";
+    this.userSurname = "Soplica";
     this.profileDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum congue facilisis.';
   }
 
   switchProfileTab(selectedTab: number){
     this.selectedProfileTab = selectedTab;
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Zdjęcie profilowe',
+      buttons: [{
+        text: 'Wybierz z galerii',
+        icon: 'photos',
+        handler: () => {
+          const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.FILE_URI,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+          }
+          this.camera.getPicture(options).then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64 (DATA_URL):
+            let base64Image = 'data:image/jpeg;base64,' + imageData;
+           }, (err) => {
+            // Handle error
+           });
+        }
+      }, {
+        text: 'Zrób zdjęcie',
+        icon: 'videocam',
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
