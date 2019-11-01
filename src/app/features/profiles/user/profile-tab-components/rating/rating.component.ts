f7cc6ee1da7bcdd501a0d24717e66abfb63c49cf
SzymonSt <szymonst2808@gmail.com>
2019-11-01 22:54:51 +0100

    Rating module
    -added rating stars

--- src/app/features/profiles/user/profile-tab-components/rating/rating.component.ts
+++ src/app/features/profiles/user/profile-tab-components/rating/rating.component.ts
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rating.component.scss'],
 })
 export class RatingComponent implements OnInit {
-
+  public starRate: number;
   constructor() { }
 
   ngOnInit() {}
 
+  starRated(star: number) {
+    this.starRate = star;
+    console.log(this.starRate);
+  }
 }
