305224317e4f8852b2e96f42e0e3f0b115e9eb1e
SzymonSt <szymonst2808@gmail.com>
2019-11-28 15:39:00 +0100

    conversation view added

--- src/app/features/profiles/user_param/chat/chat/chat.module.ts
+++ src/app/features/profiles/user_param/chat/chat/chat.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule} from '@angular/router';
 import { ChatComponent } from '../chat.component';
 import { IonicModule } from '@ionic/angular';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ConversationComponent } from '../conversation/conversation.component';
 
 
 const routes: Routes = [
-  {path: '', component: ChatComponent}
+  {path: '', component: ChatComponent},
+  {path: 'conversation', component: ConversationComponent}
 ];
 
 @NgModule({
   declarations: [
     ChatComponent,
+    ConversationComponent,
   ],
   imports: [
     CommonModule,
