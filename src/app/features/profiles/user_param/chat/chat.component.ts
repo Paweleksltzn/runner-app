7ee77fb8a21bba3817b7b2b2ceaf4576e275cfa8
SzymonSt <szymonst2808@gmail.com>
2019-11-28 22:52:49 +0100

    last changes

--- src/app/features/profiles/user_param/chat/chat.component.ts
+++ src/app/features/profiles/user_param/chat/chat.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
-
+  public privateConversations = [
+    {whoTexted: 'Mietek', lastMessage: 'Idziesz na trening?', isReaded: false}, 
+    {whoTexted: 'Marcin', lastMessage: 'Idziesz na trening?', isReaded: true},
+    {whoTexted: 'Anna', lastMessage: 'Idziesz na trening?', isReaded: true},
+  ];
   constructor() { }
 
   ngOnInit() {}
