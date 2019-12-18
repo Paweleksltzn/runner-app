c86ddb35dea3d36511b97a07a29abf09b01e9eda
Paweł <pawelantolak@interia.eu>
2019-12-18 15:26:41 +0100

    users searcher applied

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ const jwtManager = require('./auth-guards/jwt-managment');
 const authRouter = require('./routes/auth');
 const workoutRouter = require('./routes/workout');
 const notificationRouter = require('./routes/notification');
+const searchersRouter = require('./routes/searcher');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 app.use('/api/auth', authRouter); 
 app.use('/api/workout', jwtManager.jwtVerivier, workoutRouter); 
 app.use('/api/notification', jwtManager.jwtVerivier, notificationRouter); 
+app.use('/api/searchers', searchersRouter);
 
 mongoose
   .connect(credentials.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
---
+++ server/controlers/searcher.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+
+exports.searchUsers = async function(req, res, next) {
+    try {
+        let searchString = req.query.searchString;
+        let users;
+        if (searchString.split(' ').length === 2) {
+            const name = searchString.split(' ')[0];
+            const surname = searchString.split(' ')[1];
+            users =  await User.find({
+                name: new RegExp(name, 'i'),
+                surname: new RegExp(surname, 'i')
+            })
+        } else {
+            searchString = searchString.split(' ').join('');
+            users = await User.find({ nameAndSurname: new RegExp(searchString, 'i') })
+        }
+        const usersToReturn = users.map(user => {
+            return {
+                email: user.email,
+                name: user.name,
+                surname: user.surname,
+                isMale: user.isMale
+            }
+        });
+        return res.json(usersToReturn);
+    } catch {
+        return res.status(500).send('Wystąpił błąd podczas wyszukiwania użytkownika');
+    }
+    
+}
