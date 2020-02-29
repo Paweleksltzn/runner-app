943ee2b7360a341e3ac85cac04067f15779ae655
SzymonSt <szymonst2808@gmail.com>
2020-02-29 21:15:56 +0100

    Update profile.component.ts

--- src/app/features/profiles/user_param/profile/profile.component.ts
+++ src/app/features/profiles/user_param/profile/profile.component.ts
@@ -118,6 +118,7 @@ export class ProfileComponent implements OnInit {
     },
     err=>{
       this.toastGeneratorService.presentToast('Wystąpił bład podczas usuwania zdjęcia profilowego', 'danger');
+      alert(JSON.stringify(err));
     })
     
   }
