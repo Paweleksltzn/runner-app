d1ed5c5fbca17f6d3ed6a499a50d964b899e0ce0
SzymonSt <szymonst2808@gmail.com>
2020-04-14 16:13:04 +0200

    basic policy finished

--- src/app/core/auth/registration/policy/policy.component.ts
+++ src/app/core/auth/registration/policy/policy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-policy',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PolicyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() {}
 
+  public dismissModal() {
+    this.modalController.dismiss({});
+  }
 }
