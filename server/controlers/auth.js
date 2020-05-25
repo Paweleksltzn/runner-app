d5986e707525c7c4c41db9397d45efb0c25c6cf7
Paweł <pawelantolak@interia.eu>
2020-05-25 09:27:17 +0200

    is active swap

--- server/controlers/auth.js
+++ server/controlers/auth.js
@@ -41,7 +41,7 @@ exports.registerUser = async function(req, res, next) {
             nameAndSurname: req.body.name + req.body.surname,
             accessLevel: 1,
             confirmationToken,
-            isActive: true,
+            isActive: false,
             userProfile
         });
         userProfile.save();
