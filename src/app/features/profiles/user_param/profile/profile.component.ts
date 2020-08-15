de779c024de37a7be111cd859076d6981e42210e
SzymonSt <48925621+SzymonSt@users.noreply.github.com>
2020-08-15 14:29:49 +0200

    style changes

--- src/app/features/profiles/user_param/profile/profile.component.ts
+++ src/app/features/profiles/user_param/profile/profile.component.ts
@@ -112,7 +112,7 @@ export class ProfileComponent implements OnInit {
       value: 'image',
       },{
         element: 'style',
-        value: 'border-radius: 50%'
+        value: 'border-radius: 50%',  
       });
   }
 
